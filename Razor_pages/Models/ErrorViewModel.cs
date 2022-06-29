54a4782d1ac8fed69a50ca8b3689bde96d14333c
boris_from_south <sashaborisenko@tut.by>
2022-06-29 11:04:48 +0300

    Init: Upload project
    Upload project

---
+++ Razor_pages/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Razor_pages.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
